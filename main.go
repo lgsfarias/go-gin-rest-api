39f0edb8982a7bda19787b1b9493121e286dc8a2
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-11-04 11:15:43 -0300

    feat: create first route

--- main.go
+++ main.go
@@ -4,7 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func showAllStudents(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"students": []string{"John", "Paul", "George", "Ringo"},
+	})
+}
+
 func main() {
 	r := gin.Default()
+	r.GET("/students", showAllStudents)
 	r.Run(":8080")
 }
